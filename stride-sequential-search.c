22d5e463558dfbae0d19bac748f552575ab9d9e3
dlacksdn <92437394+dlacksdn@users.noreply.github.com>
2025-07-15 13:54:25 +0900

    확장성 공사 바꾸기 전

--- stride-sequential-search.c
+++ stride-sequential-search.c
@@ -1,34 +1,38 @@
 #include "threads/thread.h"
+#include "devices/timer.h"
 #include <stdio.h>
 
 #define N 3
-#define RUNNING_TIME 100  // ticks (약 1초)
+#define RUNNING_TIME 500  // ticks (약 1초)
 
 int count_stride[N];
-static bool running = true;
+static volatile bool running = true;
 
-static void
+static volatile void
 thread_func_perf(void *aux) {
   int id = *(int *)aux;
     // 이 줄 추가: 현재 실행 중인 스레드에 ID 기록
   thread_current()->perf_id = id;
 
   while (running) {
-
-    thread_yield();
+    
   }
+
+  thread_exit();
 }
 
 void test_stride_sequential_search(void) {
   set_scheduler(SCHED_STRIDE_SEQ);
 
+  int ticket_num[3] = {10, 20, 40};
+
   for (int i = 0; i < N; i++)
     count[i] = 0;
 
   static int id[N] = {0,1,2};
-  thread_create_stride("thread0", PRI_DEFAULT, 10, thread_func_perf, &id[0]);
-  thread_create_stride("thread1", PRI_DEFAULT, 20, thread_func_perf, &id[1]);
-  thread_create_stride("thread2", PRI_DEFAULT, 40, thread_func_perf, &id[2]);
+  thread_create_stride("thread 0", PRI_DEFAULT, 10, thread_func_perf, &id[0]);
+  thread_create_stride("thread 1", PRI_DEFAULT, 20, thread_func_perf, &id[1]);
+  thread_create_stride("thread 2", PRI_DEFAULT, 40, thread_func_perf, &id[2]);
 
   timer_sleep(RUNNING_TIME);
   running = false;
@@ -36,5 +40,6 @@ void test_stride_sequential_search(void) {
   printf("Stride Performance Result:\n");
   for (int i = 0; i < N; i++)
     printf("thread%d (tickets=%d) ran %d times\n",
-           i, (i == 0 ? 100 : (i == 1 ? 10 : 1)), count[i]);
+           i, ticket_num[i], count_stride[i]);
 }
+
